14da5fe5927ca468184a60e97f5cec0bc7b3033f
tgiqfe <tqji31001@gmail.com>
2019-11-12 13:03:09 +0900

    first commit

---
+++ ScriptLanguage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Language> langs = DefaultLanguageSetting.Create();
+
+            Console.WriteLine(string.Join(", ", langs));
+
+            Console.ReadLine();
+        }
+    }
+}
