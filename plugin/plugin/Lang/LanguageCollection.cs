61e0a372b549c3c56e871cf80269d1abd9a5c37c
tgiqfe <tqji31001@gmail.com>
2022-09-25 16:44:58 +0900

    Update LanguageCollection class for JsonSerialize.

--- plugin/plugin/Lang/LanguageCollection.cs
+++ plugin/plugin/Lang/LanguageCollection.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.IO;
-using System.Diagnostics;
-
+﻿
 namespace plugin.Lang
 {
     internal class LanguageCollection : List<Language>
@@ -19,9 +10,9 @@ namespace plugin.Lang
             List<Language> list = null;
             try
             {
-                using (var sr = new StreamReader(path, Encoding.UTF8))
+                using (var sr = new StreamReader(path, System.Text.Encoding.UTF8))
                 {
-                    list = JsonSerializer.Deserialize<List<Language>>(sr.ReadToEnd());
+                    list = System.Text.Json.JsonSerializer.Deserialize<List<Language>>(sr.ReadToEnd());
                 }
             }
             catch { }
@@ -37,20 +28,22 @@ namespace plugin.Lang
 
         public void Save(string path)
         {
-            string parent = System.IO.Path.GetDirectoryName(path);
-            if (!System.IO.Directory.Exists(parent))
+            string parent = Path.GetDirectoryName(path);
+            if (parent != "" && !Directory.Exists(parent))
             {
-                System.IO.Directory.CreateDirectory(parent);
+                Directory.CreateDirectory(parent);
             }
-
             try
             {
-                using (var sw = new StreamWriter(path, false, Encoding.UTF8))
+                using (var sw = new StreamWriter(path, false, System.Text.Encoding.UTF8))
                 {
-                    string json = JsonSerializer.Serialize(this, new JsonSerializerOptions()
+                    string json = System.Text.Json.JsonSerializer.Serialize(this, new System.Text.Json.JsonSerializerOptions()
                     {
+                        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                        IgnoreReadOnlyProperties = true,
+                        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
                         WriteIndented = true,
-                        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+
                     });
                     sw.WriteLine(json);
                 }
@@ -72,7 +65,7 @@ namespace plugin.Lang
             return null;
         }
 
-        public Process GetProcess(string filePath)
+        public System.Diagnostics.Process GetProcess(string filePath)
         {
             if (File.Exists(filePath))
             {
